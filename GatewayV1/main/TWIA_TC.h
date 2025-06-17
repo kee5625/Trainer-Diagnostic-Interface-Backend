dee4dce05e13b2b2eccea992b5bde310e5f551c8
nBatcher <nbatcher@atechtraining.com>
2025-06-17 12:48:26 -0400

    Reuploading

---
+++ GatewayV1/main/TWIA_TC.h
@@ -0,0 +1,12 @@
+
+
+#ifndef TWIA_TC
+#define TWIA_TC
+
+static void twai_receive_task(void *arg);
+static void twai_transmit_task(void *arg);
+static void trouble_code_task(void *arg);
+void twai_TC_Get();
+
+
+#endif
